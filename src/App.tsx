a2740b4bc7aa5fc11c7f27ce734023a6e8bbfdb0
Lenny-Prosper <leonardmalcolm00@gmail.com>
2023-03-09 03:43:29 +0000

    Mobile NavBar (Finished Basics)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import HomePage from './Home/Home';
-import FetchData from './General-Components/Fetch-data';
+// import HomePage from './Home/Home';
+// import FetchData from './General-Components/Fetch-data';
 
 function App() {
   // const observer = new IntersectionObserver(entries => {
@@ -27,9 +27,29 @@ function App() {
       dot.style.opacity = "0";
     })
   });
-  let cars = 1;
 
-  const { data: person, loading, error } = FetchData("http://localhost:8000/designers")
+  const openMenuButton = document.querySelector(".nav-icon .fa-bars") as HTMLElement,
+  closeMenuButton = document.querySelector(".nav-icon .fa-xmark") as HTMLElement,
+  mobileMenu = document.querySelector(".mobile-nav-menu") as HTMLElement,
+  menuBackground = document.querySelector(".mobile-menu-background") as HTMLElement;
+
+  openMenuButton.addEventListener("click", () => {
+    openMenuButton.style.display = "none";
+    mobileMenu.style.display = "block";
+    closeMenuButton.style.display = "block";
+    menuBackground.style.display = "block";
+    menuBackground.style.animation = "menuBackgroundIntro 0.5s linear 0s 1 normal forwards";
+  });
+  closeMenuButton.addEventListener("click", () => {
+    mobileMenu.style.display = "none";
+    closeMenuButton.style.display = "none";
+    openMenuButton.style.display = "block";
+    menuBackground.style.animation = "menuBackgroundOutro 0.5s linear 0s 1 normal forwards";
+    setInterval(() => {
+      menuBackground.style.display = "none";
+    }, 500);
+  });
+
   
   return (
     <div className="App">
@@ -37,31 +57,79 @@ function App() {
         <div className="nav-bar">
           <img src="GenImages\Spotify Logo.svg" alt="" />
 
-          <div className="nav-links">
+          <div className="desktop-nav-menu">
             <div className="nl-link">
+              <div className="nl-dot"></div>
               <div className="nl-link-title">Stories</div>
-              <div className="nl-dot"></div>
             </div>
 
             <div className="nl-link">
+              <div className="nl-dot"></div>
               <div className="nl-link-title">Listen</div>
-              <div className="nl-dot"></div>
             </div>
 
             <div className="nl-link">
+              <div className="nl-dot"></div>
               <div className="nl-link-title">Team</div>
-              <div className="nl-dot"></div>
             </div>
 
             <div className="nl-link">
-              <div className="nl-link-title">Tools</div>
               <div className="nl-dot"></div>
+              <div className="nl-link-title">Tools</div>
+            </div>
+          </div>
+
+          <div className="nav-icon">
+            <i className="fa-solid fa-bars"></i>
+            <i className="fa-solid fa-xmark"></i>
+          </div>
+        </div>
+      </div>
+
+      <div className="mobile-menu-background"></div>
+      <div className="mobile-nav-menu">
+        <div className="links">
+            <div className="nl-link">
+              <div className="nl-dot"></div>
+              <div className="nl-link-title">Stories</div>
+            </div>
+
+            <div className="nl-link">
+              <div className="nl-dot"></div>
+              <div className="nl-link-title">Listen</div>
+            </div>
+
+            <div className="nl-link">
+              <div className="nl-dot"></div>
+              <div className="nl-link-title">Team</div>
+            </div>
+
+            <div className="nl-link">
+              <div className="nl-dot"></div>
+              <div className="nl-link-title">Tools</div>
+            </div>
+        </div>
+        <div className="flex-wrapper">
+          <div className="flex">
+            <div className="socials">
+              <div>Instagram</div>
+              <div>Twitter</div>
+            </div>
+            <div className="place-time">
+              <div className="place">
+                <div>SE</div>
+                <div>UK</div>
+                <div>US</div>
+              </div>
+              <div className="time">
+                <div>06:09:28</div>
+                <div>05:09:28</div>
+                <div>12:09:28</div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <i className="fa-solid fa-bars"></i>
-
     </div>
   );
 }
